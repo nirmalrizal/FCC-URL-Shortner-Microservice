3d23577600614afb39ec516a93c4d0d78c3d9138
nirmalrizal53 <nirmalrijal41@gmail.com>
2016-09-23 20:39:16 +0545

    Fixed DB issue

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 var mongodb = require('mongodb');
+var validate = require('url-validator');
 var MongoClient = mongodb.MongoClient;
 var dbUrl = 'mongodb://urlhero:654321@ds021326.mlab.com:21326/url-database';
 
 var port = process.env.PORT || 3000;
 
+//View engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//listen app to certain port
 app.listen(port,function(err){
   if(err){
     console.log('PORT error :' + err);
@@ -14,39 +21,39 @@ app.listen(port,function(err){
   }
 });
 
+//Render HomePage
+app.get('/',function(req,res){
+  res.render('index');
+})
+
+//take url for shortening
 app.get('/new/:url*',function(req,res){
   var originalUrl = req.url.slice(5);
-  var hostname = 'https://hidden-peak-34480.herokuapp.com';
-  console.log(originalUrl);
-  var shortUrl = hostname+'/'+randNum();
-  saveUrl(originalUrl,shortUrl);
-  console.log(shortUrl);
+  if(validate(originalUrl)){ //validatin input url
+
+    var hostname = 'https://hidden-peak-34480.herokuapp.com';
+    var nuu = randNum();
+    var shortUrl = hostname+'/'+nuu;
+    saveUrl(originalUrl,shortUrl,nuu);
+    res.json({
+      "Orginal URL" : originalUrl,
+      "Short URL" : shortUrl
+    });
+
+  } else {
+    res.json({
+      "URL ERROR" : "Wrong url format, make sure you have a valid protocol and real site."
+    });
+  }
   res.end();
 });
 
-/*app.get('/:inputUrl*',function(req,res){
-  var input = req.hostname + req.path;
-  var urlFromDb = searchDB(input);
-  console.log(input);
-  console.log(urlFromDb);
-  res.redirect(urlFromDb);
-  res.end();
+//take shortUrl as input
+app.get('/:inputUrl',function(req,res){
+  var input = req.path.slice(1);
+  searchDB(input,res);
 });
-*/
 
-// redirects to the original url
-app.get('/:id',function(req,res,next){
-   if(req.params.id !== "favicon.ico") {
-   	console.log(req.params.id);
-    Url.find({shoUrl: Number(req.params.id)},function(err, url){
-         if(err) throw err;
-         res.redirect(url[0].orgiUrl);
-       });
-   }
-   else {
-   res.send('Error');
- }
-});
 
 //generate ramdom number
 function randNum(){
@@ -54,36 +61,46 @@ function randNum(){
 }
 
 //save URL to database
-function saveUrl(originalUrl,shortUrl){
+function saveUrl(originalUrl,shortUrl,n){
   MongoClient.connect(dbUrl,function(err,db){
     if(err){
       console.log('DB error : '+ err);
     } else {
       console.log('DB connection established');
-      var collection = db.collection('storeurl');
+      var collection = db.collection('test');
       collection.insert({
         "orgiUrl": originalUrl,
-        "shoUrl" : shortUrl
-      },function(err){
-        console.log('Inserted');
+        "shoUrl" : shortUrl,
+        "urlNum" : n
+      },function(err,doc){
+        db.close();
       });
     }
   });
 }
 
 //search database for url
-function searchDB(queryUrl){
+function searchDB(numb,res){
   MongoClient.connect(dbUrl,function(err,db){
     if(err){
-      console.log('Searching DB error :' + err);
+      console.log('Search database err :-- '+err);
     } else{
-      console.log('Ready for search');
-      var collection = db.collection('storeurl');
-      var q = queryUrl;
-      console.log(q);
-      collection.find({shoUrl : queryUrl }).toArray(function(err, results){
-          console.log(results); // output all records
-          return(results.orgiUrl);
+      var collection = db.collection('test');
+      collection.findOne({"urlNum" : numb},function(err,doc){
+        if(err){
+          console.log('Find error :-- '+err);
+        } else {
+          if(doc){
+            console.log('Redirecting to :-- '+doc.orgiUrl);
+            res.redirect(doc.orgiUrl);
+            db.close();
+          } else {
+            res.json({
+              "ERROR" : "This url is not in the database"
+            });
+          }
+
+        }
       });
     }
   });
